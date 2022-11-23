14291fa70fbfc35c5749e022e11cd7c7b925fdb2
Kevin Glisson <kevgliss@gmail.com>
2022-11-23 15:25:44 -0800

    bolt.py

--- src/dispatch/plugins/dispatch_slack/socket_mode.py
+++ src/dispatch/plugins/dispatch_slack/socket_mode.py
@@ -1,96 +1,13 @@
 import logging
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-import asyncio
-from fastapi import BackgroundTasks
+from dispatch.plugins.dispatch_slack.bolt import app
 
-from slack_sdk.web.async_client import AsyncWebClient
-
-from dispatch.plugins.dispatch_slack.menus import handle_slack_menu
-
-from .actions import handle_slack_action
-from .commands import handle_slack_command
-from .events import handle_slack_event, EventEnvelope
 
 log = logging.getLogger(__name__)
 
 
-async def run_websocket_process(config):
-    import time
-    from slack_sdk.socket_mode.aiohttp import SocketModeClient
-    from slack_sdk.socket_mode.response import SocketModeResponse
-    from slack_sdk.socket_mode.request import SocketModeRequest
-    from slack_bolt.response import BoltResponse
-    from slack_bolt.app.async_app import AsyncApp
-    from slack_bolt.adapter.socket_mode.async_internals import (
-        send_async_response,
-        run_async_bolt_app,
-    )
-
-    # Initialize SocketModeClient with an app-level token + WebClient
-    client = SocketModeClient(
-        # This app-level token will be used only for establishing a connection
-        app_token=config.socket_mode_app_token.get_secret_value(),  # xapp-A111-222-xyz
-        # You will be using this WebClient for performing Web API calls in listeners
-        web_client=AsyncWebClient(
-            token=config.api_bot_token.get_secret_value()
-        ),  # xoxb-111-222-xyz
-    )
-
-    app = AsyncApp(
-        token=config.socket_mode_app_token.get_secret_value(),
-        client=AsyncWebClient(token=config.api_bot_token.get_secret_value()),
-    )
-
-    async def bolt_process(client: SocketModeClient, req: SocketModeRequest):
-        start = time()
-        bolt_resp: BoltResponse = await run_async_bolt_app(app, req)
-        await send_async_response(client, req, bolt_resp, start)
-
-    async def legacy_process(client: SocketModeClient, req: SocketModeRequest):
-        background_tasks = BackgroundTasks()
-        if req.type == "events_api":
-            response = await handle_slack_event(
-                config=config,
-                client=client.web_client,
-                event=EventEnvelope(**req.payload),
-                background_tasks=background_tasks,
-            )
-
-        if req.type == "slash_commands":
-            response = await handle_slack_command(
-                config=config,
-                client=client.web_client,
-                request=req.payload,
-                background_tasks=background_tasks,
-            )
-
-        if req.type == "interactive":
-            if req.payload["type"] == "block_suggestion":
-                response = await handle_slack_menu(
-                    config=config,
-                    client=client.web_client,
-                    request=req.payload,
-                )
-
-            else:
-                response = await handle_slack_action(
-                    config=config,
-                    client=client.web_client,
-                    request=req.payload,
-                    background_tasks=background_tasks,
-                )
-
-        response = SocketModeResponse(envelope_id=req.envelope_id, payload=response)
-        await client.send_socket_mode_response(response)
-
-        # run the background tasks
-        await background_tasks()
-
-    # Add a new listener to receive messages from Slack
-    # You can add more listeners like this
-    client.socket_mode_request_listeners.append(bolt_process)
-    client.socket_mode_request_listeners.append(legacy_process)
-
-    # Establish a WebSocket connection to the Socket Mode servers
-    await client.connect()
-    await asyncio.sleep(float("inf"))
+def run_websocket_process(config):
+    app._token = config.api_bot_token.get_secret_value()
+    handler = SocketModeHandler(app, config.socket_mode_app_token.get_secret_value())
+    handler.start()
