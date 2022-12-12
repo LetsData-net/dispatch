89f8c80e23d8f04c5f526f39d7eed3e5d051288c
Kevin Glisson <kevgliss@gmail.com>
2022-12-12 15:32:39 -0800

    Adds fast api support

--- src/dispatch/plugins/dispatch_slack/bolt.py
+++ src/dispatch/plugins/dispatch_slack/bolt.py
@@ -5,6 +5,7 @@ from typing import Dict, Any, Optional
 from slack_bolt.app.async_app import AsyncApp
 from slack_bolt.response import BoltResponse
 from slack_bolt.request import BoltRequest
+from slack_bolt.adapter.starlette.async_handler import AsyncSlackRequestHandler
 
 
 from fastapi import APIRouter
@@ -31,56 +32,7 @@ async def errors(error, body, context, logger, respond):
     pprint(body)
 
 
-async def to_bolt_request(
-    req: Request,
-    body: bytes,
-    addition_context_properties: Optional[Dict[str, Any]] = None,
-) -> BoltRequest:
-    request = BoltRequest(
-        body=body.decode("utf-8"),
-        query=req.query_params,
-        headers=req.headers,
-    )
-    if addition_context_properties is not None:
-        for k, v in addition_context_properties.items():
-            request.context[k] = v
-    return await request
-
-
-async def to_starlette_response(bolt_resp: BoltResponse) -> Response:
-    resp = Response(
-        status_code=bolt_resp.status,
-        content=bolt_resp.body,
-        headers=bolt_resp.first_headers_without_set_cookie(),
-    )
-    for cookie in bolt_resp.cookies():
-        for name, c in cookie.items():
-            resp.set_cookie(
-                key=name,
-                value=c.value,
-                max_age=c.get("max-age"),
-                expires=c.get("expires"),
-                path=c.get("path"),
-                domain=c.get("domain"),
-                secure=True,
-                httponly=True,
-            )
-    return await resp
-
-
-class SlackRequestHandler:
-    def __init__(self, app: AsyncApp):  # type: ignore
-        self.app = app
-
-    async def handle(
-        self, req: Request, addition_context_properties: Optional[Dict[str, Any]] = None
-    ) -> Response:
-        body = await req.body()
-        bolt_resp = self.app.dispatch(to_bolt_request(req, body, addition_context_properties))
-        return to_starlette_response(bolt_resp)
-
-
-handler = SlackRequestHandler(app)
+handler = AsyncSlackRequestHandler(app)
 
 
 @router.post(
