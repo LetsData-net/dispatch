4a2311dc8c7a906b41eb3c644b790f07ddf3abe4
Victor Ananyev <vindex10@gmail.com>
2024-07-28 13:14:31 +0000

    remove limit on email message size

--- src/dispatch/messaging/strings.py
+++ src/dispatch/messaging/strings.py
@@ -748,7 +748,7 @@ def render_message_template(message_template: List[dict], **kwargs):
 
             # NOTE: we truncate the string to 2500 characters
             # to prevent hitting limits on SaaS integrations (e.g. Slack)
-            d["text"] = d["text"] if len(d["text"]) <= 2500 else d["text"][:2500]
+            # d["text"] = d["text"] if len(d["text"]) <= 2500 else d["text"][:2500]
 
         # render a new button array given the template
         if d.get("buttons"):
