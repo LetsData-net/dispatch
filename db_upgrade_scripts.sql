46965c0cb20b95f36b702cc84a53ee5ae29d3d3f
philipshurpik <philip.shurpik@gmail.com>
2025-03-11 10:56:27 +0100

    create new indexes to database

---
+++ db_upgrade_scripts.sql
@@ -0,0 +1,34 @@
+CREATE INDEX IF NOT EXISTS incident_name_index
+  ON dispatch_organization_default.incident (name);
+
+CREATE INDEX IF NOT EXISTS incident_reporter_id_index
+  ON dispatch_organization_default.incident (reporter_id);
+
+CREATE INDEX IF NOT EXISTS incident_project_id_reported_at_index
+  ON dispatch_organization_default.incident (project_id, reported_at DESC);
+
+CREATE INDEX IF NOT EXISTS incident_commander_id_reported_at_index
+  ON dispatch_organization_default.incident (commander_id, reported_at DESC);
+
+CREATE INDEX IF NOT EXISTS incident_visibility_reported_at_index
+  ON dispatch_organization_default.incident (visibility, reported_at DESC);
+
+CREATE INDEX IF NOT EXISTS incident_duplicate_id_index
+  ON dispatch_organization_default.incident (duplicate_id);
+
+CREATE INDEX IF NOT EXISTS participant_incident_id_index
+  ON dispatch_organization_default.participant (incident_id DESC);
+
+CREATE INDEX IF NOT EXISTS participant_individual_contact_id_index
+  ON dispatch_organization_default.participant (individual_contact_id);
+
+CREATE INDEX IF NOT EXISTS individual_contact_email_index
+  ON dispatch_organization_default.individual_contact (email);
+
+CREATE INDEX IF NOT EXISTS participant_role_participant_id_index
+  ON dispatch_organization_default.participant_role (participant_id ASC);
+
+CREATE INDEX IF NOT EXISTS incident_cost_incident_id_created_at_index
+  ON dispatch_organization_default.incident_cost (incident_id, created_at);
+
+
