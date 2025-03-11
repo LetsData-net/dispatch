ba49dbed6a79ec58e9c01336d9267ad51e92ed30
philipshurpik <philip.shurpik@gmail.com>
2025-03-11 13:42:16 +0100

    updated extra db indexes

--- db_upgrade_scripts.sql
+++ db_upgrade_scripts.sql
@@ -1,8 +1,8 @@
 CREATE INDEX IF NOT EXISTS incident_name_index
   ON dispatch_organization_default.incident (name);
 
-CREATE INDEX IF NOT EXISTS incident_reporter_id_index
-  ON dispatch_organization_default.incident (reporter_id);
+CREATE INDEX IF NOT EXISTS incident_reporter_id_reported_at_index
+  ON dispatch_organization_default.incident (reporter_id, reported_at DESC);
 
 CREATE INDEX IF NOT EXISTS incident_project_id_reported_at_index
   ON dispatch_organization_default.incident (project_id, reported_at DESC);
@@ -30,5 +30,8 @@ CREATE INDEX IF NOT EXISTS participant_role_participant_id_index
 
 CREATE INDEX IF NOT EXISTS incident_cost_incident_id_created_at_index
   ON dispatch_organization_default.incident_cost (incident_id, created_at);
+
+CREATE INDEX IF NOT EXISTS incident_type_exclude_from_metrics_idx
+  ON dispatch_organization_default.incident_type (exclude_from_metrics);
 
 
